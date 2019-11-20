307a597d7b2da9e89335a243f8c5fc7377c3d877
Neil Campbell <neildcampbell@hotmail.com>
2019-11-20 05:56:14 +0000

    added interface and buttons to display page

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
     var thermostat = new Thermostat()
+    $("#energyUsage").text(thermostat.energyUsage)
     $("#temperature").text(thermostat.temperature)
+
+    $("#increaseTemp").click(function(){
+        thermostat.increase()
+        thermostat.checkEnergyUsage()
+        $("#temperature").text(thermostat.temperature)
+        $("#energyUsage").text(thermostat.energyUsage)
+    })
+
+    $("#decreaseTemp").click(function(){
+        thermostat.decrease()
+        thermostat.checkEnergyUsage()
+        $("#temperature").text(thermostat.temperature)
+        $("#energyUsage").text(thermostat.energyUsage)
+    })
+
+    $("#powerSaveOn").click(function(){
+        thermostat.powerSavingModeOn()
+    })
+
+    $("#powerSaveOff").click(function(){
+        thermostat.powerSavingModeOff()
+    })
+
+    
+    
+    
 })
